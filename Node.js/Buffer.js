394d7220e4243cbf455e2ab01bf6163cd1c10da4
zhuzhihao <zhuzhihao@cvte.com>
2019-11-19 19:19:43 +0800

    [add][pkg.module和webpack、rollup对比]

--- Node.js/Buffer.js
+++ Node.js/Buffer.js
@@ -1,6 +1,15 @@
 const fs = require("fs");
 const text = fs.readFileSync("./data.txt"); // text => buffer
 
+// const buffer = new ArrayBuffer(8);
+// console.log("buffer", buffer);
+
+var buf = Buffer.from("211sadjfksdjfkljsdfkljsdklfj1", "base64");
+
+console.log("buffer", buf.length);
+
+console.log("bufArr", buf.buffer);
+
 // console.log("utf8");
 // console.log(text.toString("utf8"));
 
@@ -10,14 +19,14 @@ const text = fs.readFileSync("./data.txt"); // text => buffer
 // console.log("是否相等");
 // console.log(text.toString("utf8") === text.toString("base64"));
 
-var utf8Str = text.toString("utf8"); // buffer => str
-var base64Str = text.toString("base64"); // buffer => str
+// var utf8Str = text.toString("utf8"); // buffer => str
+// var base64Str = text.toString("base64"); // buffer => str
 
-const buf = Buffer.from(utf8Str, "base64"); // utf8 str => buffer base64
-console.log("buf", buf.toString("base64")); // base64buffer => base64 str
-console.log("base64Str", base64Str);
+// const buf = Buffer.from(utf8Str, "base64"); // utf8 str => buffer base64
+// console.log("buf", buf.toString("base64")); // base64buffer => base64 str
+// console.log("base64Str", base64Str);
 
-console.log(buf.toString("base64") === base64Str);
+// console.log(buf.toString("base64") === base64Str);
 
 // const buf = Buffer.from(text, "base64");
 
---
+++ 项目构建/pkg.module.md
@@ -0,0 +1,6 @@
+# package 中的 module
+
+1. ES5 的 CommonJS 模块规范，是实现不了 Tree Shaking 的（为什么？因为“得益于 javascript 灵活的访问对象属性方式”，exports 对象并不清楚到底哪些暴露方法被使用到了，于是全部代码都包含在 bundle 中）
+2. 得益于 ES6 模块规范的定义，`import`和`export`都是静态的，于是在编译阶段即可清晰知道哪些方式被使用到，故可 Tree Shaking
+3. pkg.main 放的是基于 es5 模块规范的 es5 语法的代码；而 pkg.module 则使用的是基于 es6 模块规范的 es5 语法的模块
+4. pkg.module 是一种对 pkg.main 的补充增强：对于支持 Tree Shaking 的打包工具，优先使用 pkg.module 的版本，启动 Tree Shaking，而对于不支持的，则使用 pkg.main（向后兼容）
